f6f2c8d2d640139819bfe052d1c4f2a4c48aedf5
GerardoXL <leon.capo2000@gmail.com>
2024-04-26 20:56:35 -0300

    Avances del ejercicio 1 paquete model

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -1,5 +1,28 @@
 package ar.edu.unju.fi.ejercicio1.model;
 
 public class Producto {
+	
+	private String codigo;
+	private String descripcion;
+	private double precio_unitario;
+	private String origen_fabricacion;
+	private String categoria;
+	
+	
+	public enum Origen_Fabricacion{
+		
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+		
+		
+	}
+	
+	public enum Categoria{
+		
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+		
+	}
+	
+	
+	
 
 }
