871b8869b0bbeac37e0db3348fb91a686db1dbe3
GerardoXL <leon.capo2000@gmail.com>
2024-04-26 23:57:27 -0300

    Se completa el ejercicio1 de los paquetes main y model

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/main/Main.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/main/Main.java
@@ -1,10 +1,145 @@
 package ar.edu.unju.fi.ejercicio1.main;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio1.model.Producto;
+import ar.edu.unju.fi.ejercicio1.model.Producto.Categoria;
+import ar.edu.unju.fi.ejercicio1.model.Producto.Origen_Fabricacion;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		ArrayList<Producto> productos = new ArrayList<>();
+		int opcion;
+		
+		do {
+			System.out.println("----Menu----");
+			System.out.println("1 - Crear Producto ");
+			System.out.println("2 - Mostrar Productos ");
+			System.out.println("3 - Modificar producto ");
+			System.out.println("4 - Salir ");
+			 opcion = entrada.nextInt();
+			
+			
+		    switch (opcion) {
+			case 1:
+				
+				System.out.println("ingresar codigo del prodcuto: ");
+				String codigo = entrada.nextLine();
+				entrada.nextLine();
+				
+				System.out.println("ingresar descripcion del producto: ");
+				String descripcion = entrada.nextLine();
+				
+				System.out.println("ingresar precio unitario del producto: ");
+				double precio_unitario = entrada.nextDouble();
+				
+				System.out.println("ingresar origen de fabricacion: 1. ARGENTINA, 2. CHINA, 3. BRASIL, 4. URUGUAY ");
+				int origen_fabricacion = entrada.nextInt();
+				
+				entrada.nextLine();
+				
+				System.out.println("ingresar categoria del producto: 1. TELEFONIA, 2. INFORMATICA, 3. ELECTRO HOGAR, 4. HERRAMIENTAS ");
+				int categoria = entrada.nextInt();
+				
+				entrada.nextLine();
+				
+				
+				Producto.Categoria ProductoCategoria = Producto.Categoria.values()[categoria -1];
+				Producto.Origen_Fabricacion productoOrigen = Producto.Origen_Fabricacion.values()[origen_fabricacion -1];
+				
+				productos.add(new Producto(codigo, descripcion, precio_unitario, productoOrigen, ProductoCategoria));
+				
+				
+			break;
 
+			case 2:
+				
+				for (Producto producto : productos) {
+					
+					System.out.println("Codigo : " + producto.getCodigo());
+					System.out.println("Descripcion: " + producto.getDescripcion());
+					System.out.println("Precio Unitario: " + producto.getPrecio_unitario() + "$");
+					System.out.println("Origen de Fabricacion: " + producto.getOrigen_fabricacion());
+					System.out.println("Categoria: " + producto.getCategoria());
+					System.out.println("................");
+					
+					}
+				
+				break;
+				
+			case 3:
+				
+				
+				System.out.println("ingresar el codigo del producto a modificar: ");
+				entrada.nextLine();
+				
+				String ModifiCodigo = entrada.nextLine();
+				
+				for(Producto producto : productos) {
+					
+					if(producto.getCodigo().equals(ModifiCodigo)) {
+						
+						
+					System.out.println("ingresar la nueva descripcion del producto: ");
+					
+					String DescripcionNueva = entrada.nextLine();
+					
+					producto.setDescripcion(DescripcionNueva);
+					
+					System.out.println("ingresar nuevo precio unitario del producto: ");
+					double NuevoPrecio = entrada.nextDouble();
+					
+					entrada.nextLine();
+					
+					System.out.println("elegir el nuevo origen de fabricacion 1. ARGENTINA, 2. CHINA, 3. BRASIL, 4:URUGUAY: ");
+					
+					int OrigenNuevo = entrada.nextInt();
+					
+					entrada.nextLine();
+					
+					Producto.Origen_Fabricacion nuevoOrigen_Fabricacion = Producto.Origen_Fabricacion.values()[OrigenNuevo -1];
+					
+					producto.setOrigen_fabricacion(nuevoOrigen_Fabricacion);
+					
+					System.out.println("elegir la nueva categoria del producto: 1. TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS: ");
+					
+					int CategoriaNueva = entrada.nextInt();
+					entrada.nextLine();
+					
+					Producto.Categoria nuevCategoria = Producto.Categoria.values()[CategoriaNueva -1];
+					
+						
+					}
+					
+					
+					
+				}
+		
+		    
+		      break;
+		      
+			case 4:
+				
+				System.out.println("Salir....");
+				
+				break;
+				
+				default:
+				
+				System.out.println("Opcion invalida. eliga una opcion del menu.");
+			
+		    }
+		    
+		} while(opcion !=4);
+		
+		
+		entrada.close();
+		
+		
 	}
-
 }
--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -5,8 +5,18 @@ public class Producto {
 	private String codigo;
 	private String descripcion;
 	private double precio_unitario;
-	private String origen_fabricacion;
-	private String categoria;
+	private Origen_Fabricacion origen_fabricacion ;
+	private  Categoria categoria;
+	
+	public Producto(String codigo, String descripcion, 
+		double precio_unitario, Origen_Fabricacion origen_fabricacion, Categoria categoria) {
+		
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precio_unitario = precio_unitario;
+		this.origen_fabricacion = origen_fabricacion;
+		this.categoria = categoria;
+		}
 	
 	
 	public enum Origen_Fabricacion{
@@ -20,7 +30,53 @@ public class Producto {
 		
 		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
 		
+		
+		//getters y setters
+		
 	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio_unitario() {
+		return precio_unitario;
+	}
+
+	public void setPrecio_unitario(double precio_unitario) {
+		this.precio_unitario = precio_unitario;
+	}
+
+	public Origen_Fabricacion getOrigen_fabricacion() {
+		return origen_fabricacion;
+	}
+
+	public void setOrigen_fabricacion(Origen_Fabricacion origen_fabricacion) {
+		this.origen_fabricacion = origen_fabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
 	
 	
 	
