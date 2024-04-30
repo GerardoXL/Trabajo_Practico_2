3723bb3b387d2ea090e1d5663d3dc2071785a649
GerardoXL <leon.capo2000@gmail.com>
2024-04-30 00:06:34 -0300

    avance del paquete main
    se agrega arraylist y las estructuras do while y switch para los casos
    de 1- crear efemeride 2- mostrar 3- eliminar 4- modificar y 5 - salir

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio2/main/Main.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio2/main/Main.java
@@ -1,10 +1,125 @@
 package ar.edu.unju.fi.ejercicio2.main;
+import ar.edu.unju.fi.ejercicio2.constantes.Mes;
+import ar.edu.unju.fi.ejercicio2.model.Efemeride;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	ArrayList<Efemeride> efemerides = new ArrayList<>();
+	
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		int opcion;
+		
+		do {
+			
+			System.out.println("Menu: ");
+			System.out.println("1- Crear efemeride ");
+			System.out.println("2- Eliminar efemeride ");
+			System.out.println("3- Eliminar efemeride");
+			System.out.println("4- Modificar efemeride");
+			System.out.println("5- Salir ");
+			
+			opcion = entrada.nextInt();
+			
+			switch (opcion) {
+			
+			case 1:
+				
+				System.out.println("ingrese la efemeride: ");
+				String efemeride = entrada.nextLine();
+				break;
+			case 2:
+				
+				System.out.println("Efemerides: ");
+				
+				for(int i = 0; i < efemerides.size(); i++) {
+					
+					System.out.println((i+1) + ". " + efemerides.get(i));
+					
+				}
+			break;
+			
+			case 3:
+				
+				System.out.println("ingresar numero de efemeride a eliminar");
+				
+				int IndiceEliminar = entrada.nextInt();
+				
+				if(IndiceEliminar >= 1 && IndiceEliminar <= efemerides.size()) {
+					
+					System.out.println("Se elimino la efemeride. ");
+					
+					
+				} else {
+					
+					System.out.println("Efemeride invalida");
+				}
+				
+				break;
+				
+			case 4:
+				
+				System.out.println("Ingresar efemeride a modificar: ");
+				
+				int IndiceModificar = entrada.nextInt();
+				
+				entrada.nextLine();
+				
+				if(IndiceModificar >= 1 && IndiceModificar <= efemerides.size()) {
+					
+					System.out.println("ingresar nueva efemeride: ");
+					
+					String detalleNuevaEfemeride = entrada.nextLine();
+					
+					Efemeride EfemerideNueva = new Efemeride();
+					
+					EfemerideNueva.setDetalle(detalleNuevaEfemeride);
+					
+					efemerides.set(IndiceModificar -1 , EfemerideNueva);
+					
+					System.out.println("Se modifico la efemeride. ");
+					
+				} else {
+					
+					System.out.println("Efemeride invalida ");
+					
+				}
+				
+			    break;
+			    
+			case 5:
+				
+				System.out.println("Saliendo del menu: ");
+				
+				break;
+				
+				default:
+					
+					System.out.println("opcion erronea. seleccione una opcion del menu. ");
+					
+					break;
+				
+			}
+			
+			
+			
+		} while (opcion !=5);
+		
+		
+		entrada.close();
 
 	}
+	
+	
+		
+		
+	
 
 }
