17d162ffc9dfc24aea2b2d40b96c947d905dbee5
GerardoXL <leon.capo2000@gmail.com>
2024-04-29 22:28:12 -0300

    Se agrega getter y setters de Efemeride

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -21,6 +21,80 @@ public class Efemeride {
 		this.mes=mes;
 		
 		
+		
+		
+	}
+
+
+
+
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+
+
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+
+
+	public int getDia() {
+		return dia;
+	}
+
+
+
+
+
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+
+
+
+
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+
+
+
+
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+
+
+
+
+
+	public Mes getMes() {
+		return mes;
+	}
+
+
+
+
+
+
+	public void setMes(Mes mes) {
+		this.mes = mes;
 	}
 	
 	
