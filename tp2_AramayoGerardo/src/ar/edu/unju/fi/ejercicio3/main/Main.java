3e4f39a164091ca9a27a3eb95394dc0ae2a0b4cc
GerardoXL <leon.capo2000@gmail.com>
2024-05-06 02:10:25 -0300

    se agrega cambios en paquete main ejercicio3
    se agrega una estructura for para recorrer los elementos del array de
    provincia declaradas en la clase enum agregando agregando la cantidad de
    poblacion, superficie y por ultimo la densidad

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -8,7 +8,14 @@ public class Main {
 		
         Provincia[] provincias = Provincia.values();
         
-		
+        for (int i = 0; i < provincias.length; i++) {
+    		
+			System.out.println("Provincia: " + provincias[i]);
+			System.out.println("Poblacion: " + provincias[i].getCantidad_poblacion());
+			System.out.println("Superficie: " + provincias[i].getSuperficie() + " km");
+			System.out.println("Densidad de poblacion: " + provincias[i].CaluloDensidadPoblacion() + " hab/km2");
+			System.out.println("*******************************");
+		}
 
 	}
 
