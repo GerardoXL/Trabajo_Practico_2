6edfb2dbb7e51d97f05e7f0670ce29069fd5ee0e
GerardoXL <leon.capo2000@gmail.com>
2024-05-06 01:54:35 -0300

    se agrego cambios en en el paquete main ejercicio3
    se agrego un Array de la clase enum Provincia
    en el paquete main.

--- tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2_AramayoGerardo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,8 +1,13 @@
 package ar.edu.unju.fi.ejercicio3.main;
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+        Provincia[] provincias = Provincia.values();
+        
 		
 
 	}
